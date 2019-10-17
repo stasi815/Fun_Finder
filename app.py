b85641a328dea0cb175084fb1d05c18768d570bb
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-16 22:57:31 -0700

    Users can see all events

--- app.py
+++ app.py
@@ -6,4 +6,13 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     return render_template('home.html', msg='Flask is cool!')
-    
+
+events = [
+    { 'title': 'Storytime', 'description': 'Free storytime' },
+    { 'title': 'Movie in the park', 'description': 'Cinderella' }
+]
+
+@app.route('/events')
+def events_index():
+    """Show all events."""
+    return render_template('events_index.html', events=events)
