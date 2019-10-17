099a6402a77d65eb32cb699a53d266fcdb6fe72a
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-16 23:15:39 -0700

    added MongoDB and events db

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Fun_Finder
+events = db.events
 
 app = Flask(__name__)
 
@@ -7,12 +12,12 @@ def index():
     """Return homepage."""
     return render_template('home.html', msg='Flask is cool!')
 
-events = [
-    { 'title': 'Storytime', 'description': 'Free storytime' },
-    { 'title': 'Movie in the park', 'description': 'Cinderella' }
-]
+#  events = [
+#     { 'title': 'Storytime', 'description': 'Free storytime' },
+#     { 'title': 'Movie in the park', 'description': 'Cinderella' }
+# ]
 
 @app.route('/events')
 def events_index():
     """Show all events."""
-    return render_template('events_index.html', events=events)
+    return render_template('events_index.html', events=events.find())
