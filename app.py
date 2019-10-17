a27438900975dda82a5ee9ce2a5efd7e6db152fc
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-17 02:39:12 -0700

    User can edit, update and delete and event

--- app.py
+++ app.py
@@ -26,13 +26,7 @@ def events_index():
 @app.route('/events/new')
 def new_event():
     """Create a new event."""
-    return render_template('new_event.html')
-
-# @app.route('/events', methods=['POST'])
-# def event_submit():
-#     """Submit a new event."""
-#     print(request.form.to_dict())
-#     return redirect(url_for('events_index'))
+    return render_template('new_event.html', event={}, title='New Event')
 
 @app.route('/events', methods=['POST'])
 def event_submit():
@@ -54,4 +48,30 @@ def show_event(event_id):
     event = events.find_one({'_id': ObjectId(event_id)})
     return render_template('show_event.html', event=event)
 
-# @app.route('/events/')
+@app.route('/events/<event_id>/edit')
+def edit_event(event_id):
+    """Show the edit form for event"""
+    event = events.find_one({'_id': ObjectId(event_id)})
+    return render_template('edit_event.html', event=event, title='Edit Event')
+
+@app.route('/events/<event_id>', methods=['POST'])
+def update_event(event_id):
+    """Submit an edited event"""
+    updated_event = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'age_range': request.form.get('age_range'),
+        'location': request.form.get('location'),
+        'date': request.form.get('date'),
+        'category': request.form.get('category')
+    }
+    events.update_one(
+        {'_id': ObjectId(event_id)},
+        {'$set': updated_event})
+    return redirect(url_for('show_event', event_id=event_id))
+
+@app.route('/events/<event_id>/delete', methods=['POST'])
+def delete_event(event_id):
+    """Delete an event"""
+    events.delete_one({'_id': ObjectId(event_id)})
+    return redirect(url_for('events_index'))
